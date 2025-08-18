bdc3d0b799fbc9a82dfc5319c93f809db8d430e3
avjiang <alvin@xilnex.com>
2025-08-18 21:28:25 +0800

    1. WIP stock receipt

---
+++ src/stock/stock-receipt.controller.ts
@@ -0,0 +1,32 @@
+import express, { NextFunction, Request, Response } from "express"
+import validator from "validator"
+import service from "./stock-receipt.service"
+import { StockReceipt } from "../../prisma/client"
+import NetworkRequest from "../api-helpers/network-request"
+import { RequestValidateError } from "../api-helpers/error"
+import { sendResponse } from "../api-helpers/network"
+import { UpdateStockChecksRequestBody } from "./stock-movement.request"
+import { AuthRequest } from "src/middleware/auth-request"
+
+const router = express.Router()
+
+let getItemStockReceipt = (req: AuthRequest, res: Response, next: NextFunction) => {
+    if (!req.user) {
+        throw new RequestValidateError('User not authenticated')
+    }
+
+    const outletId = req.query.outletId ? parseInt(req.query.outletId as string) : undefined
+    const itemId = req.query.itemId as string
+    if (!validator.isNumeric(itemId)) {
+        throw new RequestValidateError('Item ID format incorrect')
+    }
+
+    service.getItemStockReceipt(req.user.databaseName, parseInt(itemId), outletId)
+        .then((stockReceipts: StockReceipt[]) => sendResponse(res, stockReceipts))
+        .catch(next)
+}
+
+// routes
+router.get('/find', getItemStockReceipt)
+// router.put('/update', updateManyStockChecks)
+export = router
---
+++ src/stock/stock-receipt.service.ts
@@ -0,0 +1,49 @@
+import { PrismaClient, StockReceipt } from "../../prisma/client"
+import { getTenantPrisma } from '../db';
+
+let getItemStockReceipt = async (databaseName: string, itemId: number, outletId?: number) => {
+    const tenantPrisma: PrismaClient = getTenantPrisma(databaseName)
+    try {
+        const whereCondition: any = {
+            itemId: itemId,
+            deleted: false
+        }
+        if (outletId) {
+            whereCondition.outletId = outletId
+        }
+
+        const stockReceipts = await tenantPrisma.stockReceipt.findMany({
+            where: whereCondition,
+            // include: {
+            //     outlet: {
+            //         select: {
+            //             outletName: true
+            //         }
+            //     },
+            //     item: {
+            //         select: {
+            //             itemName: true,
+            //             itemCode: true
+            //         }
+            //     }
+            // },
+            orderBy: {
+                receiptDate: 'desc'
+            }
+        })
+
+        return stockReceipts.map(receipt => ({
+            ...receipt,
+            // outletName: receipt.outlet.outletName,
+            // itemName: receipt.item.itemName,
+            // itemCode: receipt.item.itemCode,
+            outlet: undefined,
+            item: undefined
+        }))
+    }
+    catch (error) {
+        throw error
+    }
+}
+
+export = { getItemStockReceipt }
